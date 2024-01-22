e2ca708f54683b443fa71e2760abf330c31429a9
AlexSche <Alexander.S_97@gmx.de>
2024-01-23 00:47:45 +0100

    readd testing to vibration

--- vibrate.py
+++ vibrate.py
@@ -2,7 +2,7 @@
 import serial
 import time
 import threading
-arduino = serial.Serial(port='COM3', baudrate=115200, timeout=.1)
+arduino = serial.Serial(port='COM4', baudrate=115200, timeout=.1)
 
 def startVibration():
     arduino.write(bytes('1', 'utf-8'))
@@ -21,3 +21,14 @@ def shortVibration():
     timer.start()
     data = arduino.readline()
     return data
+
+def write_read(x):
+    arduino.write(bytes(x, 'utf-8'))
+    time.sleep(0.05)
+    data = arduino.readline()
+    return data
+
+while True:
+    num = input("Enter 0 or 1: ") # Taking input from user
+    value = write_read(num)
+    print(value) # printing the value
